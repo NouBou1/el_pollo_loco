4e0a3e29957b1399c18f92333e914313f58922b0
Noureddin Boussaada <n.boussaada92@gmail.com>
2026-06-02 22:42:35 +0200

    Fix character and chicken y-positions for better alignment; update background layers in World

--- models/character.class.js
+++ models/character.class.js
@@ -4,7 +4,7 @@ class Character extends MovableObject {
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.x = 50; // Startposition x
-        this.y = 320; // Startposition y
+        this.y = 280; // Startposition y
     }
 
     jump() {
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,7 @@
 class Chicken extends MovableObject {
     height = 60;
     width = 60;
-    y = 400; // Feste y-Position für alle Hühner1
+    y = 370; // Feste y-Position für alle Hühner1
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
