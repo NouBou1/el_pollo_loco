f4c03ae2a4f1d83cf41504d7162abee262c88d2e
Noureddin Boussaada <n.boussaada92@gmail.com>
2026-07-01 13:58:34 +0200

    feat: update offset values for Bottle and Coin classes, and adjust font sizes in styles for improved readability

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -7,7 +7,7 @@ class Bottle extends DrawableObject {
     y = 360;
     height = 60;
     width = 60;
-    offset = { top: 10, left: 15, right: 15, bottom: 5 };
+    offset = { top: 18, left: 27, right: 27, bottom: 12 };
 
     /**
      * Creates a bottle at a given or random horizontal position.
